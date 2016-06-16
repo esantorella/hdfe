665a205597172e5bfe508f4a48c66570b60b0d58
lizs <elizabeth.santorella@gmail.com>
2016-06-15 21:10:51 -0400

    It works now!

--- tests.py
+++ tests.py
@@ -11,12 +11,17 @@ def test_hdfe():
     fes1 = np.random.gamma(1, 1, k1)
     fes2 = np.random.gamma(1, 1, k2)
     
-    z = np.array(np.random.normal(0, 1, n))
+    z = np.random.normal(0, 1, n)
     cat1 = np.random.choice(k1, n)
     cat2 = np.random.choice(k2, n)
-    y = z * beta + fes1[cat1] + fes2[cat2] + np.random.normal(0, 20, n)
+    y = z * beta + fes1[cat1] + fes2[cat2]
+    print('y')
+    print(y)
+    print('categorical data')
+    print(np.vstack((cat1, cat2)).T)
               
-    beta, fixed_effects = estimate_coefficients(y, z, np.vstack((cat1, cat2)).T)
+    beta, fixed_effects = estimate_coefficients(y, z, np.vstack((cat1, cat2)).T, 'alternating projections')
+    print(beta)
     return
     
 if __name__ == "__main__":
