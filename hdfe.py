b5115b23490dcd0adc5b5653bdbe69d6c5987c75
Ben Thompson <t.ben.thompson@gmail.com>
2016-06-17 21:00:46 -0400

    Fixed fencepost problem in Groupby

--- hdfe.py
+++ hdfe.py
@@ -11,38 +11,38 @@ class Groupby:
     @profile
     def __init__(self, keys):
         _, self.keys_as_int = np.unique(keys, return_inverse = True)
-        self.n_keys = max(self.keys_as_int)
+        self.n_keys = max(self.keys_as_int) + 1
         self.set_indices()
-        
+
     @profile
     def set_indices(self):
-        self.indices = [[] for i in range(self.n_keys+1)]
+        self.indices = [[] for i in range(self.n_keys)]
         for i, k in enumerate(self.keys_as_int):
             self.indices[k].append(i)
         self.indices = [np.array(elt) for elt in self.indices]
-        
-    @profile  
+
+    @profile
     def apply(self, function, vector):
         result = np.zeros(len(vector))
         for k in range(self.n_keys):
             result[self.indices[k]] = function(vector[self.indices[k]])
-        return result 
+        return result
 
 @profile
 def estimate_with_alternating_projections(y, z, categorical_data):
     z = np.expand_dims(z, 1)
-    n_cols =  z.shape[1] 
+    n_cols =  z.shape[1]
     n, num_fes = categorical_data.shape
-    
+
     grouped = [Groupby(categorical_data[:, i]) for i in range(num_fes)]
     z_projected = np.zeros(z.shape)
-    
+
     # Project each column of z onto dummies
     for col in range(n_cols):
         fixed_effects = np.zeros((n, num_fes))
         ssr = np.dot(z[:, col], z[:, col])
         ssr_last = 10 * ssr
-        
+
         while (ssr_last - ssr) / ssr_last > 10**(-5):
             ssr_last = ssr
             residual = z[:, col] - np.sum(fixed_effects, 1)
@@ -51,30 +51,30 @@ def estimate_with_alternating_projections(y, z, categorical_data):
                                      + grouped[fe].apply(np.mean, residual)
                 residual = z[:, col] - np.sum(fixed_effects, 1)
             ssr = np.dot(residual, residual)
-                
+
         z_projected[:, col] = z[:, col] - np.sum(fixed_effects, 1)
-        
+
     beta = np.linalg.lstsq(z_projected, y)[0]
 
     return beta, 1
-    
+
 def estimate_brute_force(y, z, categorical_data):
     k = z.shape[1] if len(z.shape) > 1 else 1
     print(k)
     num_fes = categorical_data.shape[1]
-    
+
     def get_dummies(v):
         _, data_as_int = np.unique(v, return_inverse = True)
         return sps.csc_matrix((np.ones(len(v)), (range(len(v)), v)))
-        
-    dummies = sps.hstack([get_dummies(categorical_data[:, i]) 
+
+    dummies = sps.hstack([get_dummies(categorical_data[:, i])
                           for i in range(num_fes)]).astype(int)
     z = sps.csc_matrix(np.expand_dims(z, 1))
     rhs = sps.hstack((z, dummies))
     params = sps_linalg.lsqr(rhs, y)[0]
     return params[:k], params[k:]
-    
-    
+
+
 def estimate_coefficients(y, z, categorical_data, method):
     if method == 'alternating projections':
         return estimate_with_alternating_projections(y, z, categorical_data)
