d8acfbd3767c1c54f70a15b4d04aeb9dfe8213cf
lizs <elizabeth.santorella@gmail.com>
2016-07-03 18:42:47 -0400

    Now takes pybind11 arrays as arguments

--- cppplay.cpp
+++ cppplay.cpp
@@ -34,28 +34,16 @@ pybind11::array_t<T> make_array(const std::vector<size_t>& shape) {
 struct groupby {
     std::vector<std::vector<int>> indices;
 
-    groupby(std::vector<int> keys) {
-        auto result_it = std::max_element(keys.begin(), keys.end());
+    groupby(pybind11::array_t<int> keys) {
+        auto keys_buf = keys.request();
+        int* keys_begin = reinterpret_cast<int*>(keys_buf.ptr);
+        int* keys_end = keys_begin + keys_buf.shape[0];
+        auto result_it = std::max_element(keys_begin, keys_end);
         int n_keys = *result_it + 1;
         indices.resize(n_keys);
-        for (size_t i = 0; i < keys.size(); i++) {
-            indices[keys[i]].push_back(i);
+        for (size_t i = 0; i < keys_buf.shape[0]; i++) {
+            indices[keys_begin[i]].push_back(i);	
         }
-    }
-
-    std::vector<double> apply(std::vector<double> y, pybind11::object f) {
-        std::vector<double> out(y.size());
-        for (size_t i = 0; i < indices.size(); i++) {
-            std::vector<double> group_input(indices[i].size());
-            for (size_t j = 0; j < indices[i].size(); j++) {
-                group_input[j] = y[indices[i][j]];
-            }
-            double group_output = f(group_input).cast<double>();
-            for (size_t j = 0; j < indices[i].size(); j++) {
-                out[indices[i][j]] = group_output;
-            }
-        }
-        return out;
     }
     
     pybind11::array_t<double> apply_2(pybind11::array_t<double> y,
@@ -101,8 +89,7 @@ struct groupby {
 PYBIND11_PLUGIN(cppplay) {
     pybind11::module m("cppplay", "auto-compiled c++ extension");
     pybind11::class_<groupby>(m, "Groupby")
-        .def(pybind11::init<std::vector<int>>())
-        .def("apply_2", &groupby::apply_2)
-        .def("apply", &groupby::apply);
+        .def(pybind11::init<pybind11::array_t<int>>())
+        .def("apply_2", &groupby::apply_2);
     return m.ptr();
 }
