c70c3b2f4efc1fd4534a281d47252dfcc969441f
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-09-26 22:25:34 -0400

    Gave Groupby its own file

--- hdfe.py
+++ hdfe.py
@@ -5,149 +5,7 @@ import scipy.linalg
 from multicollinearity import remove_collinear_cols
 from itertools import chain
 import pandas as pd
-import warnings
-
-
-class Groupby:
-    def __init__(self, keys):
-        """
-
-        :param keys: List of group identifiers. Both __init__ and apply will run
-            much faster if keys is already sorted.
-        """
-        if type(keys) in (np.ndarray, pd.Series) and \
-                np.issubdtype(keys.dtype, np.number) and np.all(np.diff(keys) >= 0):
-            if keys.ndim == 2:
-                if keys.shape[1] == 1:
-                    keys = keys[:, 0]
-                else:
-                    raise ValueError('keys should be 1-dimensional')
-            assert keys.ndim == 1
-
-            self.already_sorted = True
-            new_idx = np.concatenate(([1], np.diff(keys) != 0))
-            self.first_occurrences = np.where(new_idx)[0]
-            self.keys_as_int = np.cumsum(new_idx) - 1
-            assert isinstance(self.keys_as_int, np.ndarray)
-            self.n_keys = self.keys_as_int[-1] + 1
-
-        else:
-            self.already_sorted = False
-            _, self.first_occurrences, self.keys_as_int = \
-                np.unique(keys, return_index=True, return_inverse=True)
-            self.n_keys = max(self.keys_as_int) + 1
-        self.indices = self._set_indices()
-
-    def _set_indices(self):
-        if self.already_sorted:
-            indices = [slice(i, j) for i, j in zip(self.first_occurrences[:-1],
-                                                   self.first_occurrences[1:])]
-            assert isinstance(indices, list)
-            indices.append(slice(self.first_occurrences[-1], len(self.keys_as_int)))
-            indices = np.array(indices)
-        else:
-            indices = [[] for _ in range(self.n_keys)]
-            for i, k in enumerate(self.keys_as_int):
-                indices[k].append(i)
-            indices = np.array([np.array(elt) for elt in indices])
-        return indices
-
-    def apply(self, function_, array, broadcast=True, width=1):
-        """
-        Applies a function to each group, where groups are defined by self.keys_as_int (or, equivalently, as the
-            argument of __init__.)
-        If broadcast=True, first dimension of output will equal first dimension of "array", as in Pandas "transform".
-        If broadcast=False, first dimension of output equals self.n_keys, as in Pandas "groupby".
-
-        :param function_:
-        :param array: np.ndarray, must have same first dimension as self.keys_as_int.
-        :param broadcast: bool
-        :param width: Second dimension of output.
-        :return:
-        :rtype: np.ndarray
-        """
-        warnings.warn('apply is deprecated; use apply_2')
-        if len(array.shape) == 1:
-            array = array[:, None]
-        if broadcast:
-            result = np.zeros((array.shape[0], width))
-
-            for k in range(self.n_keys):
-                result[self.indices[k], :] = function_(array[self.indices[k], :])
-
-        else:
-            result = np.zeros((self.n_keys, width))
-
-            if self.already_sorted:
-                for k, idx in enumerate(self.indices):
-                    result[k, :] = function_(array[idx, :])
-            else:
-                for k, idx in enumerate(self.indices):
-                    result[self.keys_as_int[self.first_occurrences[k]], :] = \
-                        function_(array[idx, :])
-        return result
-
-    def apply_2(self, function_, array, broadcast=True, shape=None, order='c'):
-        """
-        Applies a function to each group, where groups are defined by self.keys_as_int (or, equivalently, as the
-            argument of __init__.)
-        If broadcast=True, first dimension of output will equal first dimension of "array", as in Pandas "transform".
-        If broadcast=False, first dimension of output equals self.n_keys, as in Pandas "groupby".
-
-        :param function_: function to be applied to each group
-        :param array: np.ndarray. Should have same first dimension as self.keys_as_int.
-        :param broadcast: bool
-        :param shape: Shape of output. Can be up to 3-dimensional.
-            First dimension must be array.shape[0] (if broadcast=True)
-            or self.n_keys (if broadcast=False). Default is for output to be one-dimensional.
-        :param order: Should output be c-ordered or fortran-ordered?
-        :return:
-        :rtype: np.ndarray
-        """
-        assert isinstance(array, np.ndarray)
-        if broadcast:
-            result = np.zeros(array.shape[0] if shape is None else shape, order=order)
-            assert result.shape[0] == array.shape[0]
-
-            # np.take doesn't allow slice arguments, so this has to be more verbose than when not already sorted
-            if self.already_sorted:
-                if array.ndim == 1:
-                    for k, idx in enumerate(self.indices):
-                        result[idx] = function_(array[idx])
-                elif array.ndim == 2:
-                    for k, idx in enumerate(self.indices):
-                        result[idx] = function_(array[idx, :])
-                elif array.ndim == 3:
-                    for k, idx in enumerate(self.indices):
-                        result[idx] = function_(array[idx, :, :])
-                else:
-                    raise NotImplementedError('Can\'t have more than 3 dims')
-            else:
-                for k, idx in enumerate(self.indices):
-                    result[idx] = function_(np.take(array, idx, 0))
-
-        else:
-            result = np.zeros(self.n_keys if shape is None else shape, order=order)
-            assert result.shape[0] == self.n_keys
-            if self.already_sorted:
-                if array.ndim == 1:
-                    for k, idx in enumerate(self.indices):
-                        result[k] = function_(array[idx])
-                elif array.ndim == 2:
-                    for k, idx in enumerate(self.indices):
-                        result[k] = function_(array[idx, :])
-                elif array.ndim == 3:
-                    for k, idx in enumerate(self.indices):
-                        result[k] = function_(array[idx, :, :])
-                else:
-                    raise NotImplementedError('Can\'t have more than 3 dims')
-
-            else:
-                for k, idx in enumerate(self.indices):
-                    result[self.keys_as_int[self.first_occurrences[k]]] \
-                        = function_(np.take(array, idx, 0))
-
-        return result
+from groupby import Groupby
 
 
 def make_dummies(elt, drop_col):
@@ -203,14 +61,14 @@ def estimate(data, y: np.ndarray, x, categorical_controls: list, check_rank=Fals
     elif len(categorical_controls) == 1:
         print('Using within estimator')
         grouped = Groupby(data[categorical_controls[0]].values)
-        x_demeaned = grouped.apply_2(lambda z: z - np.mean(z, 0), x, shape=x.shape)
+        x_demeaned = grouped.apply(lambda z: z - np.mean(z, 0), x, shape=x.shape)
         # k x n_outcomes
         b = np.linalg.lstsq(x_demeaned, y)[0]
         assert b.ndim == 2
         error = y - x.dot(b)
         assert error.shape == y.shape
         # n_teachers x n_outcomes
-        fixed_effects = grouped.apply_2(lambda arr: np.mean(arr, 0), error, broadcast=False,
+        fixed_effects = grouped.apply(lambda arr: np.mean(arr, 0), error, broadcast=False,
                                         shape=(grouped.n_keys, y.shape[1]))
         assert fixed_effects.ndim == 2
         # (n_teachers + k) x n_outcomes
@@ -264,7 +122,7 @@ def estimate(data, y: np.ndarray, x, categorical_controls: list, check_rank=Fals
 
             V = []
             for i in range(y.shape[1]):
-                u_ = grouped.apply_2(f, np.hstack((error[:, i, None], x.A)),
+                u_ = grouped.apply(f, np.hstack((error[:, i, None], x.A)),
                                      shape=(grouped.n_keys, x.shape[1]), broadcast=False)
 
                 inner = u_.T.dot(u_)
@@ -356,7 +214,7 @@ def make_lags(df, n_lags_back, n_lags_forward, outcomes, groupby,
 
         width = 2 * len(outcomes) if fill_zeros else len(outcomes)
 
-        new_data = grouped.apply_2(f, outcome_data, True, shape=(len(df), width))
+        new_data = grouped.apply(f, outcome_data, True, shape=(len(df), width))
         new_cols = [out + '_lag_' + str(lag) for out in outcomes]
         if fill_zeros:
             new_cols += [out + '_lag_' + str(lag) + '_mi'
